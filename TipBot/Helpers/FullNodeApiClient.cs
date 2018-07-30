0f9505122df6bcd8b0eea818421513d4b47546e6
derp <derp@derp.com>
2018-07-30 12:04:41 +0000

    SFN api client helper

--- TipBot/Helpers/FullNodeApiClient.cs
+++ TipBot/Helpers/FullNodeApiClient.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Text;
+using System.Web;
 
 namespace TipBot.Helpers
 {
@@ -21,38 +22,53 @@ namespace TipBot.Helpers
 			this.logger = LogManager.GetCurrentClassLogger();
 		}
 
+		private string buildUrl(string apiMethod, Dictionary<string, object> queryStringParams)
+		{
+			var builder = new UriBuilder(settings.FullNodeApiUrl + apiMethod);
+			builder.Port = settings.FullNodeApiPort;
+			if (queryStringParams != null)
+			{
+				var query = HttpUtility.ParseQueryString(builder.Query);
+				foreach (var key in queryStringParams.Keys)
+				{
+					query[key] = queryStringParams[key].ToString();
+				}
+				builder.Query = query.ToString();
+			}
+			var apiUrl = builder.ToString();
+			return apiUrl;
+		}
+
 		public string callApi(string httpMethod, string apiMethod, Dictionary<string, object> jsonParams)
 		{
 			if (!settings.FullNodeApiEnabled)
 			{
-				throw new Exception();
+				throw new Exception(); //todo, proper exception
 			}
-			var apiUrl = settings.FullNodeApiUrl + apiMethod;
+
 			string result = null;
 			using (var client = new HttpClient())
 			{
-				HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+				HttpResponseMessage response = null;
 				if (httpMethod == "GET")
 				{
+					var apiUrl = buildUrl(apiMethod, jsonParams);
 					response = client.GetAsync(apiUrl).Result;
-					if (response.IsSuccessStatusCode)
-					{
-						var responseContent = response.Content;
-					    result = responseContent.ReadAsStringAsync().Result;
-					}
 				}
 				if (httpMethod == "POST")
 				{
+					var apiUrl = buildUrl(apiMethod, null);
+					response = client.GetAsync(apiUrl).Result;
 
 					var json = JsonConvert.SerializeObject(jsonParams);
 					var data = new StringContent(json, Encoding.UTF8, "application/json");
 
 					response = client.PostAsync(apiUrl, data).Result;
-					if (response.IsSuccessStatusCode)
-					{
-						var responseContent = response.Content;
-						result = responseContent.ReadAsStringAsync().Result;
-					}
+				}
+				if (response.IsSuccessStatusCode)
+				{
+					var responseContent = response.Content;
+					result = responseContent.ReadAsStringAsync().Result;
 				}
 			}
 			return result;
@@ -69,17 +85,41 @@ namespace TipBot.Helpers
 		{
 			var apiUrl = $"SmartContracts/storage?ContractAddress={contractAddress}&StorageKey={variableName}&DataType={variableType}";
 			logger.Info(apiUrl);
-			var variableContents = callApi("GET", apiUrl, null);
+			var querystringParams = new Dictionary<string, object>
+			{
+				{"ContractAddress", contractAddress},
+				{"StorageKey", variableName},
+				{"DataType", variableType}
+			};
+			var variableContents = callApi("GET", apiUrl, querystringParams);
 			logger.Info(variableContents);
 			return variableContents;
 		}
 
-		public string callSmartContractMethod(Dictionary<string, object>  jsonParams)
+		public string callSmartContractMethod(string contractAddress, string methodName, uint amount, string senderAddress)
 		{
+			var smartContractParameters = new List<string>();
 
 			var apiUrl = $"/SmartContracts/build-and-send-call";
 			logger.Info(apiUrl);
-			var variableContents = callApi("POST", apiUrl, null);
+
+			var jsonParams = new Dictionary<string, object>
+			{
+				{"walletName", settings.WalletName},
+				{"accountName", FullNodeApiClient.AccountName},
+				{"contractAddress", contractAddress},
+				{"methodName", methodName},
+				{"amount", amount},
+				{"feeAmount", "10000"},
+				{"password", this.settings.WalletPassword},
+				{"gasPrice", 1},
+				{"gasLimit", 5000000},
+				{"sender", senderAddress},
+				{"parameters", smartContractParameters}
+			};
+
+
+			var variableContents = callApi("POST", apiUrl, jsonParams);
 			logger.Info(variableContents);
 			return variableContents;
 		}
